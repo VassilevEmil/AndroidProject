9c30b8e42b53c316b5e70c38e8c4e778e3ced164
VassilevEmil <emilvassilev5@abv.bg>
2022-11-08 19:38:42 +0100

    wip.. added Credential class which will host all the rest endpoints

--- app/src/main/java/com/example/androidproject/Model/NewsModel/NewsServiceGenerator.java
+++ app/src/main/java/com/example/androidproject/Model/NewsModel/NewsServiceGenerator.java
@@ -1,4 +1,19 @@
 package com.example.androidproject.Model.NewsModel;
 
+import com.example.androidproject.Model.utils.Credentials;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class NewsServiceGenerator {
+
+    private static Retrofit.Builder retrofitBuilder = new Retrofit.Builder().baseUrl(Credentials.BASE_URL_NEWS)
+            .addConverterFactory(GsonConverterFactory.create());
+
+    private static Retrofit retrofit = retrofitBuilder.build();
+    private static NewsApi newsApi = retrofit.create(NewsApi.class);
+
+    public static NewsApi getNewsApi(){
+        return newsApi;
+    }
 }
