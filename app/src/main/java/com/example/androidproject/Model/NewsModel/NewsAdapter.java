11870ba10804fcdf7f42d383290aab8613fe47af
Mvycas <martynas.vycas@hotmail.com>
2022-11-18 10:06:20 +0100

    UI changes.

--- app/src/main/java/com/example/androidproject/Model/NewsModel/NewsAdapter.java
+++ app/src/main/java/com/example/androidproject/Model/NewsModel/NewsAdapter.java
@@ -14,6 +14,7 @@ import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.androidproject.Entities.NewsModel;
 import com.example.androidproject.R;
 
